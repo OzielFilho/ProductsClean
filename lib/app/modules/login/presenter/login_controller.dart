dd37276da8a7cd75c51c7d46ccf9f5ee90c8daef
OzielFilho <OzielFilho03@hotmail.com>
2022-01-27 19:13:08 -0300

    feat : struct login create

---
+++ lib/app/modules/login/presenter/login_controller.dart
@@ -0,0 +1,6 @@
+import 'package:mobx/mobx.dart';
+part 'login_controller.g.dart';
+
+class Logincontroller = _LogincontrollerBase with _$Logincontroller;
+
+abstract class _LogincontrollerBase with Store {}
